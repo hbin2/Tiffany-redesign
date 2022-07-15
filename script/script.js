8b6da339942ad81d72643b586c9a1fac7b153fd5
hbin2 <andydtngpqls@naver.com>
2022-07-15 22:24:57 +0900

    메세지

--- script/script.js
+++ script/script.js
@@ -1,6 +1,12 @@
 new fullpage('#fullpage', {
-    licenseKey: 'YOUR KEY HERE',
-    
+  navigation:true,
+  navigationPosition: 'right',
+  showActiveTooltip:true,
+  responsiveWidth:1900,
+  scrollOverflow: true,
+  afterResponsive: function(isResponsive){
+    //alert("Is responsive: " + isResponsive);
+  }
   });
 
   // aos 라이브러리
