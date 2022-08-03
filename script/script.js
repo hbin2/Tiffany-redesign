28acafff109921a7abec95b43a40d767c696aeec
hbin2 <andydtngpqls@naver.com>
2022-08-04 00:25:05 +0900

    메세지

--- script/script.js
+++ script/script.js
@@ -1,14 +1,48 @@
-new fullpage('#fullpage', {
-  navigation:true,
-  navigationPosition: 'right',
-  showActiveTooltip:true,
-  responsiveWidth:1900,
-  scrollOverflow: true,
-  afterResponsive: function(isResponsive){
+new fullpage("#fullpage", {
+  navigation: true,
+  navigationPosition: "right",
+  showActiveTooltip: true,
+  responsiveWidth: 1900,
+  scrollOverflow: false,
+  afterResponsive: function (isResponsive) {
     //alert("Is responsive: " + isResponsive);
-  }
+  },
+});
+
+// aos 라이브러리
+
+AOS.init();
+
+//슬라이더
+function SliderBox__init() {
+  const swiper = new Swiper(".slider-box .swiper", {
+    // Optional parameters
+    loop: true,
+    parallax: true,
+
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+
+    watchSlidesProgress: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".slider-box .swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".slider-box .swiper-button-next",
+      prevEl: ".slider-box .swiper-button-prev",
+    },
   });
+}
 
-  // aos 라이브러리
-
-  AOS.init();
+SliderBox__init();
